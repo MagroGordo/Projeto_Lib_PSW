1f436ef9563fa895f6f7a0d16dc6532f686e4946
MagroGordo <amgrogordo123@gmail.com>
2025-11-05 00:50:30 +0000

    started search books page, ajax working

--- scripts/request-handlers.js
+++ scripts/request-handlers.js
@@ -1,15 +1,33 @@
 const mysql = require("mysql2");
-const options = require("../config/options").database;
+const options = require("../scripts/options.json").database;
 
+// devolve todos os livros
 module.exports.getBooks = (req, res) => {
-    const conn = mysql.createConnection(options);
-    var query = "SELECT * FROM books";
-    conn.query(query, (err, rows, fields) => {
-        if(err){
-            res.json({"message":"erro"});
-        } else {
-            res.json({"message":"ok","rows":rows,"fields":fields});
-        }
-    });
-    conn.end();
-}
+  var connection = mysql.createConnection(options);
+  var sql = "SELECT * FROM books";
+
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      console.error("Erro ao buscar livros:", err);
+      res.json({ message: "Erro" });
+    } else {
+      res.json({ message: "ok", data: rows });
+    }
+    connection.end();
+  });
+};
+
+module.exports.getBook = (req, res) => {
+  var connection = mysql.createConnection(options);
+  var sql = mysql.format("SELECT * FROM books WHERE id = ?", [req.params.id]);
+
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      console.error("Erro ao buscar livro:", err);
+      res.json({ message: "Erro" });
+    } else {
+      res.json({ message: "ok", data: rows });
+    }
+    connection.end();
+  });
+};
